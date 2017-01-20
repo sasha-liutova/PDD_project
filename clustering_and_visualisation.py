b73928a298f140fc88995f62d66a9aa351be8d2b
Liutova, Oleksandra <oleksandra.liutova@merck.com>
2017-01-20 17:22:32 +0100

    performed analysis

--- clustering_and_visualisation.py
+++ clustering_and_visualisation.py
@@ -4,15 +4,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle
 from sklearn.decomposition import PCA, TruncatedSVD
+import time
 
 
 def load_data(filename, n_snippets):
     """
-    Loads 'percentage' % of data from file
+    Loads specified number of snippets from file
     """
     data = pickle.load(open(filename, "rb"))
-    # length = int(len(data) * percentage)
-    # data_reduced = data[: int(len(data) * percentage)]
     data_reduced = data[: n_snippets]
     return data_reduced
 
@@ -104,11 +103,16 @@ def visualise(data, labels):
 
     # calculate how many clusters there are
     unique_labels = []
+    counters = {}
     for l in labels:
         if not l in unique_labels:
             unique_labels.append(l)
+            counters[l] = 0
+        else:
+            counters[l] += 1
     n_clusters = len(unique_labels)
     print('# of clusters: ', n_clusters)
+    unique_labels = sorted(counters, key=counters.get, reverse=True)
 
     # normalizing clusters' numbers fo colors assigning
     minimum = min(unique_labels)
@@ -120,8 +124,8 @@ def visualise(data, labels):
         plot_data.append([point, label])
 
     # list of colors for visualization
-    colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c',
-              '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
+    colors = ['#a6cee3', '#33a02c', '#fdbf6f', '#b2df8a', '#fb9a99', '#cab2d6', '#ffff99',
+              '#1f78b4', '#e31a1c', '#ff7f00', '#6a3d9a', '#b15928']
     if n_clusters > len(colors):
         color = iter(plt.cm.rainbow(np.linspace(0, 1, n_clusters)))
         for i in range(n_clusters - len(colors)):
@@ -131,7 +135,8 @@ def visualise(data, labels):
     # displaying plot
     for element in plot_data:
         point, label = element[0], element[1]
-        plt.scatter(point[0], point[1], color=colors[label], s=5)
+        index = unique_labels.index(label)
+        plt.scatter(point[0], point[1], color=colors[index], s=3)
     plt.show()
 
 
@@ -144,28 +149,37 @@ def apply_TruncatedSVD(data):
 
 
 if __name__ == "__main__":
-    n_clusters, n_snippets = 10, 100000
-    eps = 20
+    n_clusters, n_snippets = 40, 20000
 
     print('Loading data from files...')
-    features = load_data('features_bag_of_words.dat', n_snippets)
-    snippets = load_data('snippets.dat', n_snippets)
+    t0 = time.time()
+    features = load_data('data/features_tf_idf.dat', n_snippets)
+    snippets = load_data('data/snippets.dat', n_snippets)
+    print(time.time() - t0, ' s')
 
     print('Performing clustering...')
-    # labels = cluster_minibatchkmeans(features, n_clusters)
-    # pickle.dump(labels, open('labels_BoW_minibatchKM_' + str(n_snippets) + '_' + str(n_clusters) + '.dat', "wb"))
-    labels = pickle.load(open('labels_BoW_minibatchKM_' + str(n_snippets) + '_' + str(n_clusters) + '.dat', "rb"))
+    t0 = time.time()
+    labels = cluster_k_means(features, n_clusters)
+    pickle.dump(labels, open('data/labels_tf_idf_' + str(n_clusters) + '.dat', "wb"))
+    print(time.time() - t0, ' s')
+    # labels = pickle.load(open('data/labels_tf_idf_' + str(n_clusters) + '.dat', "rb"))
 
     print('Saving clustered data...')
-    save_clustered_snippets(snippets, labels, n_clusters, 'clusters_BoW_minibatchKM_' + str(n_snippets) + '_' + str(n_clusters) + '.txt')
+    t0 = time.time()
+    save_clustered_snippets(snippets, labels, n_clusters, 'data/clusters_tf_idf_' + str(n_clusters) + '.txt')
+    print(time.time() - t0, ' s')
 
     print('Applying Truncated SVD...')
+    t0 = time.time()
     reduced_features = apply_TruncatedSVD(features)
+    print(time.time() - t0, ' s')
 
     print('Transforming data to 2D space...')
+    t0 = time.time()
     transformed_features = reduce_dimensionality(reduced_features)
-    pickle.dump(transformed_features, open('data_vizual_BoW_' + str(n_snippets) + '.dat', "wb"))
-    # transformed_features = pickle.load(open('data_vizual_BoW_' + str(n_snippets) + '.dat', "rb"))
+    pickle.dump(transformed_features, open('data/data_vizual_tf_idf.dat', "wb"))
+    print(time.time() - t0, ' s')
+    # transformed_features = pickle.load(open('data/data_vizual_tf_idf.dat', "rb"))
 
     print('Visualizing data...')
     visualise(transformed_features, labels)
--- preprocessing.py
+++ preprocessing.py
@@ -14,22 +14,22 @@ def load_snippets(query_text):
                                   host='52.214.158.93', database='so')
     cursor = cnx.cursor()
     cursor.execute(query_text)
-    snippets, i = [], 0
+    snippets, snippets_trash, i = [], [], 0
     time0 = time()
     for post in cursor:
         snippets_current = extract_code(post[0])
         for snippet in snippets_current:
             if is_meaningful(snippet):
+                # snippets.append({'snippet': snippet, 'features': [post[1], post[2]]})
                 snippets.append(snippet)
+            else:
+                snippets_trash.append(snippet)
         i += 1
         if i % 10000 == 0:
-            print(i, ' posts processed, ', (time()-time0)/(i/10000), ' s')
-        # if i % 130000 == 0:
-        #     save_data(snippets, 'snippets' + str(i/130000) + '.dat')
-        #     print('snippets' + str(i/130000) + '.dat saved')
-        #     snippets = []
-
-    print('Number of posts retrieved: ', i, ', number of snippets: ', len(snippets))
+            print(i, ' posts processed, ', (time()-time0), ' s')
+            time0 = time()
+    print('Number of posts retrieved: ', i, ', number of snippets: ', len(snippets), ', filtered out snippets: ', len(snippets_trash))
+    save_data(snippets_trash, 'data/snippets_trash.dat')
     cursor.close()
     cnx.close()
     return snippets
@@ -46,7 +46,7 @@ def extract_code(post):
         start += len(start_str)
         end = post.find(end_str, start)
         if end > 0:
-            snippets.append(post[start : end].replace('&#xA;', '\n'))
+            snippets.append(post[start : end].replace('&#xA;', '\n').replace('&gt;', '>').replace('&lt;', '<'))
         start = post.find(start_str, end)
     return snippets
 
@@ -88,7 +88,7 @@ def extract_features_bag_of_words(snippets):
     vectorizer = CountVectorizer(min_df=2) # a word has to occur in at least 2 documents to be added to vocabulary
     features = vectorizer.fit_transform(snippets)
     vocabulary = str(vectorizer.vocabulary_)
-    with open('vocabulary_Bag_of_Words_mindf1_Full.txt', mode='w', encoding='utf-8') as a_file:
+    with open('data/vocabulary_Bag_of_Words_mindf1_Full.txt', mode='w', encoding='utf-8') as a_file:
             a_file.write(vocabulary)
     return features
 
@@ -102,7 +102,7 @@ def extract_features_tf_idf(snippets):
     vectorizer = TfidfVectorizer(min_df=2)
     features = vectorizer.fit_transform(snippets)
     vocabulary = str(vectorizer.vocabulary_)
-    with open('vocabulary_Tf_idf_mindf2_Full.txt', mode='w', encoding='utf-8') as a_file:
+    with open('data/vocabulary_Tf_idf_mindf2_Full.txt', mode='w', encoding='utf-8') as a_file:
             a_file.write(vocabulary)
     return features
 
@@ -111,13 +111,55 @@ def save_data(data, filename):
     pickle.dump(data, open(filename, "wb"))
 
 
+def substitute_symbols(snippets):
+    new_snippets = []
+    for snippet in snippets:
+        new_snippet = snippet.replace('&gt;', '>').replace('&lt;', '<')
+        new_snippets.append(new_snippet)
+    return new_snippets
+
+
+def filter_out_xml(snippets):
+    new_snippets, filtered_out = [], []
+    for snippet in snippets:
+        if snippet[0] in ['<', '>'] or snippet[-1] in ['<', '>']:
+            filtered_out.append(snippet)
+        else:
+            new_snippets.append(snippet)
+    print('# filtered out xml: ', len(filtered_out), ', left: ', len(new_snippets))
+    pickle.dump(filtered_out, open('data/filtered_out_xml_snippets.dat', "wb"))
+    return new_snippets
+
+
 if __name__ == "__main__":
 
+    t0 = time()
     print('Retrieving data from database...')
-    snippets = load_snippets("SELECT Body FROM js_posts_full")
-    print('Saving snippets...')
-    save_data(snippets, 'snippets.dat')
-    print('Extracting features from snippets...')
-    features = extract_features_bag_of_words(snippets)
-    print('Saving features...')
-    save_data(features, 'features_bag_of_words.dat')
+    snippets_raw = load_snippets("SELECT Body FROM js_posts_full")
+    print(time() - t0, ' s')
+    # snippets = load_snippets("SELECT Body, Score, ViewCount, AnswerCount, CommentCount FROM js_posts_full")
+    # print('Saving snippets...')
+    # save_data(snippets, 'snippets_extended.dat')
+    # snippets = pickle.load(open('data/snippets.dat', "rb"))
+
+    print('Filtering out XML snippets...')
+    t0 = time()
+    snippets = filter_out_xml(snippets_raw)
+    print('After filtering out XML ', len(snippets), ' out of ', len(snippets_raw), ' left.')
+    print(time() - t0, ' s')
+
+    print('Extracting tf_idf features from snippets...')
+    t0 = time()
+    features1 = extract_features_tf_idf(snippets)
+    print(time() - t0, ' s')
+
+    # print('Saving features...')
+    # save_data(features1, 'data/features_tf_idf.dat')
+
+    print('Extracting bag_of_words features from snippets...')
+    t0 = time()
+    features2 = extract_features_bag_of_words(snippets)
+    print(time() - t0, ' s')
+
+    # print('Saving features...')
+    # save_data(features2, 'data/features_bag_of_words.dat')
